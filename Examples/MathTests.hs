985a0a2b6bc135990401cd570292454b8120764b
Pepijn  van den Ende <PS.vandenEnde@student.han.nl>
2024-02-27 09:15:25 +0100

    progress
    still figuring it out

--- Examples/MathTests.hs
+++ Examples/MathTests.hs
@@ -2,20 +2,7 @@ module Main (main) where
 
 --run all the functions in the main function`
 main :: IO ()
-main = do
-  putStrLn "Hello, Haskell!"
-  putStrLn "The 5th fibonacci number is: "
-  print(fibonacci 5)
-  putStrLn "The factorial of 5 is: "
-  print(factorial 5)
-  putStrLn "The golden ratio is: "
-  print goldenRatio
-  putStrLn "Is 5 a prime number? "
-  print(isPrime 5)
-  putStrLn "The greatest common divisor of 5 and 10 is: "
-  print(greatestCommonDivisor 5 10)
-  putStrLn "The smallest common multiple of 30 and 20 is: "
-  print(smallestCommonMultiple 30 20)
+main = print(isEven 2)
 
 fibonacci :: Int -> Int
 fibonacci 0 = 0
@@ -29,16 +16,6 @@ factorial n = n * factorial(n - 1)
 goldenRatio :: Float
 goldenRatio = (1 + sqrt 5)/2
 
-isPrime :: Int -> Bool
-isPrime n  | n < 2 = False
-           | otherwise = null [ x |  x <- [2.. isqrt n], n `mod` x == 0]
-      where isqrt = truncate . (sqrt :: Double -> Double) . fromIntegral
-  
-greatestCommonDivisor :: Int -> Int -> Int
-greatestCommonDivisor a 0 = a 
-greatestCommonDivisor a b = greatestCommonDivisor b (a `mod` b)
-
-smallestCommonMultiple :: Int -> Int -> Int
-smallestCommonMultiple a b = a * b `div` greatestCommonDivisor a b
-
+isEven :: Int -> Bool
+isEven a -> a mod 2 == 0 -> True
 
--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,30 @@
 module Main (main) where
 
+import Data.List (isInfixOf, elemIndices)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = print (regexMatch "(color)" "The color green")
+
+regexMatch :: String -> String -> Bool
+regexMatch regex input | regex == input = True
+                       | containsSubstring regex input = True
+                       | xor (containsSubstring regex input) (extractAndMatchSubstring regex input) = True
+                       | otherwise = False
+
+containsSubstring :: String -> String -> Bool
+containsSubstring substring string = substring `isInfixOf` string
+
+extractAndMatchSubstring :: String -> String -> Bool
+extractAndMatchSubstring regex input 
+    | containsSubstring "(" regex && containsSubstring ")" regex = 
+        let start = head (elemIndices '(' regex) + 1
+            end = head (elemIndices ')' regex)
+            substring = take (end - start) (drop start regex)
+        in containsSubstring substring input
+    | otherwise = False
+
+xor :: Bool -> Bool -> Bool
+xor a b = a /= b
 
 
 
